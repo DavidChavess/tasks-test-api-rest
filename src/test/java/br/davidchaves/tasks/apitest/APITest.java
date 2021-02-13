44c8699cac7adfd10cb2133a943ae5d1318695a5
David Chaves Ferreira <davi.ch.fe@gmail.com>
2021-02-13 17:19:11 -0300

    Adicionando testes de API com RestAssured

--- src/test/java/br/davidchaves/tasks/apitest/APITest.java
+++ src/test/java/br/davidchaves/tasks/apitest/APITest.java
@@ -1,11 +1,55 @@
 package br.davidchaves.tasks.apitest;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 
 public class APITest {
 
-    @Test
-    public void teste(){
-
+    @BeforeClass
+    public static void setUp(){
+        RestAssured.baseURI = "http://localhost:8001/tasks-backend";
     }
+
+    @Test
+    public void deveRetornarTarefas(){
+        RestAssured.given()
+            .when()
+                .get("/todo")
+            .then()
+                .statusCode(200)
+            ;
+    }
+
+    @Test
+    public void deveSalvarTarefaComSucesso(){
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body("{\"task\":\"teste de api\",\"dueDate\":\"2021-11-10\"}")
+            .when()
+                .post("/todo")
+            .then()
+                .statusCode(201)
+            ;
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaComDataPassada(){
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body("{\"task\":\"teste de api\",\"dueDate\":\"2015-11-10\"}")
+            .when()
+                .post("/todo")
+            .then()
+                .statusCode(400)
+                .body("message", is("Due date must not be in past"));
+            ;
+    }
+
+
+
 }
